a122c50feae178680e7dea703175e77a46c44485
RaissaOliv <raissa130704@gmail.com>
2025-04-04 11:36:23 -0300

    ye

--- GRPC/client.js
+++ GRPC/client.js
@@ -11,7 +11,8 @@ const packageDefinition = protoLoader.loadSync('calculator.proto', {
 });
 const calculatorProto = grpc.loadPackageDefinition(packageDefinition).calculator;
 
-const client = new calculatorProto.Calculator('localhost:50051', grpc.credentials.createInsecure());
+// Create gRPC client
+const client = new calculatorProto.Calculator('13.218.165.32:50051', grpc.credentials.createInsecure());
 
 
 client.Sum({ a: 7, b: 5 }, (error, response) => {
